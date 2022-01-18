5fa3175cbc98f5699b2e81b253c548092f9f0cff
Mikita Kukavenka <godinfrog@gmail.com>
2022-01-18 19:36:34 +0300

    added routes

--- src/app/api/services/languages-evolution.service.ts
+++ src/app/api/services/languages-evolution.service.ts
@@ -28,6 +28,117 @@ export class LanguagesEvolutionService extends BaseService {
   }
 
   /**
+   * Path part for operation getAllLanguagesFrom
+   */
+  static readonly GetAllLanguagesFromPath = '/api/evolve/allfrom/{fromId}';
+
+  /**
+   * Get all languages to which path from the given language is possible.
+   *
+   *
+   *
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `getAllLanguagesFrom()` instead.
+   *
+   * This method doesn't expect any request body.
+   */
+  getAllLanguagesFrom$Response(params: {
+    fromId: number;
+  }): Observable<StrictHttpResponse<Array<Language>>> {
+
+    const rb = new RequestBuilder(this.rootUrl, LanguagesEvolutionService.GetAllLanguagesFromPath, 'get');
+    if (params) {
+      rb.path('fromId', params.fromId, {});
+    }
+
+    return this.http.request(rb.build({
+      responseType: 'json',
+      accept: 'application/json'
+    })).pipe(
+      filter((r: any) => r instanceof HttpResponse),
+      map((r: HttpResponse<any>) => {
+        return r as StrictHttpResponse<Array<Language>>;
+      })
+    );
+  }
+
+  /**
+   * Get all languages to which path from the given language is possible.
+   *
+   *
+   *
+   * This method provides access to only to the response body.
+   * To access the full response (for headers, for example), `getAllLanguagesFrom$Response()` instead.
+   *
+   * This method doesn't expect any request body.
+   */
+  getAllLanguagesFrom(params: {
+    fromId: number;
+  }): Observable<Array<Language>> {
+
+    return this.getAllLanguagesFrom$Response(params).pipe(
+      map((r: StrictHttpResponse<Array<Language>>) => r.body as Array<Language>)
+    );
+  }
+
+  /**
+   * Path part for operation getAllRoutes
+   */
+  static readonly GetAllRoutesPath = '/api/evolve/routes/{fromId}/{toId}';
+
+  /**
+   * Get all routes from language to other given language.
+   *
+   *
+   *
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `getAllRoutes()` instead.
+   *
+   * This method doesn't expect any request body.
+   */
+  getAllRoutes$Response(params: {
+    fromId: number;
+    toId: number;
+  }): Observable<StrictHttpResponse<Array<Array<Language>>>> {
+
+    const rb = new RequestBuilder(this.rootUrl, LanguagesEvolutionService.GetAllRoutesPath, 'get');
+    if (params) {
+      rb.path('fromId', params.fromId, {});
+      rb.path('toId', params.toId, {});
+    }
+
+    return this.http.request(rb.build({
+      responseType: 'json',
+      accept: 'application/json'
+    })).pipe(
+      filter((r: any) => r instanceof HttpResponse),
+      map((r: HttpResponse<any>) => {
+        return r as StrictHttpResponse<Array<Array<Language>>>;
+      })
+    );
+  }
+
+  /**
+   * Get all routes from language to other given language.
+   *
+   *
+   *
+   * This method provides access to only to the response body.
+   * To access the full response (for headers, for example), `getAllRoutes$Response()` instead.
+   *
+   * This method doesn't expect any request body.
+   */
+  getAllRoutes(params: {
+    fromId: number;
+    toId: number;
+  }): Observable<Array<Array<Language>>> {
+
+    return this.getAllRoutes$Response(params).pipe(
+      map((r: StrictHttpResponse<Array<Array<Language>>>) => r.body as Array<Array<Language>>)
+    );
+  }
+
+  /**
    * Path part for operation trace
    */
   static readonly TracePath = '/api/evolve/trace/{word}';
--- src/app/components/selectors/trace-language-recursive/trace-language-recursive.component.ts
+++ src/app/components/selectors/trace-language-recursive/trace-language-recursive.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Language} from '../../../api/models/language';
 import {LanguagesService} from '../../../api/services/languages.service';
+import {LanguagesEvolutionService} from '../../../api/services/languages-evolution.service';
 
 @Component({
   selector: 'app-trace-language-recursive',
@@ -10,11 +11,12 @@ import {LanguagesService} from '../../../api/services/languages.service';
 export class TraceLanguageRecursiveComponent implements OnInit {
 
   languages: Language[] = [];
+  languagesRoutes: Language[][][] = [];
   availableLanguagesTo: Language[][] = [];
   @Output() routeChanged: EventEmitter<Language[]> = new EventEmitter<Language[]>();
 
 
-  constructor(private languagesService: LanguagesService) {
+  constructor(private languagesService: LanguagesService, private languagesEvolutionService: LanguagesEvolutionService) {
   }
 
   ngOnInit(): void {
@@ -28,10 +30,17 @@ export class TraceLanguageRecursiveComponent implements OnInit {
     console.log('changeLanguage', langId, newLang, this.availableLanguagesTo);
     this.availableLanguagesTo.splice(langId + 1, this.availableLanguagesTo.length - langId - 1);
     this.languages.splice(langId + 1, this.availableLanguagesTo.length - langId - 1);
+    this.languagesRoutes.splice(langId + 1, this.availableLanguagesTo.length - langId - 1);
     console.log('changeLanguage', langId, newLang, this.availableLanguagesTo);
-    this.languagesService.getAllLanguagesFrom({fromId: newLang.id}).subscribe(languages => {
+    this.languagesEvolutionService.getAllLanguagesFrom({fromId: newLang.id}).subscribe(languages => {
       this.availableLanguagesTo[langId + 1] = languages;
     });
+    if (langId > 0) {
+      this.languagesEvolutionService.getAllRoutes({fromId: this.languages[langId - 1].id, toId: newLang.id}).subscribe(languages => {
+        console.log('changeLanguage', languages);
+        this.languagesRoutes[langId] = languages;
+      });
+    }
   }
 
 }
