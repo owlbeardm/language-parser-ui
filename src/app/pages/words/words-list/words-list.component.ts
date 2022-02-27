80d60177cf4bdc9548571909d1ac2eca4c92e917
c7d5a6 <godinfrog@gmail.com>
2022-02-27 23:29:32 +0300

    changed word service

--- src/app/pages/words/words-list/words-list.component.ts
+++ src/app/pages/words/words-list/words-list.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {WordsService} from '../../../api/services/words.service';
-import {Word} from '../../../api/models/word';
 import {PageResultWord} from '../../../api/models/page-result-word';
 
 @Component({
@@ -12,6 +11,8 @@ export class WordsListComponent implements OnInit {
 
   words: PageResultWord = {};
   page = 0;
+  size = 30;
+  pageBttns: number[] = [];
 
   constructor(private wordService: WordsService) {
   }
@@ -21,12 +22,36 @@ export class WordsListComponent implements OnInit {
   }
 
   load(): void {
-    this.wordService.getAllWords({filter: {page: this.page, size: 30}}).subscribe(
+    this.wordService.getAllWords({filter: {page: this.page, size: this.size}}).subscribe(
       (words) => {
         if (words.data) {
           this.words = words;
+          this.redrawPageBttns();
         }
       }
     );
+  }
+
+  changePage(newpagenmb: number | undefined): void {
+    if (this.words?.totalPages && newpagenmb !== undefined) {
+      const page = Math.min(Math.max(0, newpagenmb), this.words.totalPages - 1);
+      if (page !== this.page) {
+        this.page = page;
+        this.load();
+      }
+    }
+  }
+
+  redrawPageBttns(): void {
+    if (this.words.totalPages) {
+      this.pageBttns = [];
+      for (let i = 0; i < 3; i++) {
+        this.pageBttns.push(i);
+        this.pageBttns.push(this.words.totalPages - 1 - i);
+        this.pageBttns.push(this.page + i);
+        this.pageBttns.push(this.page - i);
+      }
+      this.pageBttns = this.pageBttns.filter((v, i, a) => a.indexOf(v) === i && v >= 0 && this.words.totalPages && v < this.words.totalPages).sort((a, b) => a - b);
+    }
   }
 }
