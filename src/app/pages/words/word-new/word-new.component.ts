b4c63d483cfb8e4217492a2dfc7194d317f233a2
Mikita Kukavenka <godinfrog@gmail.com>
2022-05-12 18:13:58 +0300

    adding word changes

--- src/app/pages/words/word-new/word-new.component.ts
+++ src/app/pages/words/word-new/word-new.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Language} from '../../../api/models/language';
+import {LanguageConnectionType} from '../../../api/models/language-connection-type';
+import {Pos} from '../../../api/models/pos';
+import {PosService} from '../../../api/services/pos.service';
 
 @Component({
   selector: 'app-word-new',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./word-new.component.css']
 })
 export class WordNewComponent implements OnInit {
+  language?: Language;
+  languageTo?: Language;
+  languageConnectionType = LanguageConnectionType;
+  typeKeys = Object.keys(LanguageConnectionType);
+  type: LanguageConnectionType | string = this.typeKeys[0];
+  poses: Pos[] = [];
 
-  constructor() { }
+  constructor(private posService: PosService) {
+  }
 
   ngOnInit(): void {
+    console.log(this.type);
+    console.log(this.languageConnectionType);
+    console.log(this.typeKeys);
   }
+
+  changeLanguage(param: { langId: number | undefined }): void {
+    if (param.langId) {
+      this.posService.getAllPosByLanguage({languageId: param.langId}).subscribe(poses => {
+        this.poses = poses.sort((a, b) => a.name ? a.name.localeCompare(b.name ? b.name : '') : -1);
+      });
+    } else {
+      this.poses = [];
+    }
+  }
+
+  changeLanguageFrom(param: { languageFromId: number | undefined }): void {
+  }
+
+  typeChanged(param: { type: LanguageConnectionType }): void {
+    console.log(param.type);
+  }
+
 
 }
