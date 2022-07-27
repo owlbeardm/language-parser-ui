5a5c3db84e4d8c5931d4cff5e74d094e1351e555
Mikita Kukavenka <godinfrog@gmail.com>
2022-07-27 20:07:36 +0300

    fix

--- src/app/api/models/detailed-word.ts
+++ src/app/api/models/detailed-word.ts
@@ -1,11 +1,12 @@
 /* tslint:disable */
 /* eslint-disable */
 import { Etymology } from './etymology';
+import { Translation } from './translation';
 import { WordWithWritten } from './word-with-written';
 export interface DetailedWord {
   descendants?: Array<WordWithWritten>;
   dirived?: Array<WordWithWritten>;
   etymology?: Etymology;
-  translations?: Array<string>;
+  translations?: Array<Translation>;
   word?: WordWithWritten;
 }
